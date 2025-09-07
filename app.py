1f941072c4c8fb65319716a62af8453f18ee23fd
sarikregmi <89179475+sarikregmi@users.noreply.github.com>
2025-09-07 22:01:55 +0545

    Save current work before pulling

--- app.py
+++ app.py
@@ -1,56 +1,122 @@
-from flask import Flask, render_template,url_for,redirect,request
+from flask import Flask, render_template, url_for, redirect, request, session, flash
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 
-app=Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI']='sqlite:///test.db'
-db=SQLAlchemy(app)
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+app.secret_key = "your_secret_key"
+db = SQLAlchemy(app)
+
+
+# -------------------
+# MODELS
+# -------------------
+class Register(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20), nullable=False, unique=True)
+    email = db.Column(db.String(50), nullable=False, unique=True)
+    password = db.Column(db.String(80), nullable=False)
+
 class Todo(db.Model):
-    id=db.Column(db.Integer,primary_key=True)
-    content=db.Column(db.String(200),nullable=False)
-    complete=db.Column(db.Integer,default=0)
-    date_created=db.Column(db.DateTime,default=datetime.utcnow)
-
-    def __repr__(self):
-        return '<Task %r>' % self.id
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    complete = db.Column(db.Integer, default=0)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow)
 
 
-@app.route('/',methods=['POST','GET'])
-def hello(): 
-    if request.method=='POST':
-         task_containt=request.form['task']
-         new_task=Todo(content=task_containt)
-         try:
-                db.session.add(new_task)
-                db.session.commit()
-                return redirect('/')
-         except:
-                return 'There was an issue adding your task' 
-    else:
-            tasks=Todo.query.order_by(Todo.date_created).all()
-            return render_template('index.html',tasks=tasks)
-        
+# -------------------
+# USER AUTH ROUTES
+# -------------------
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    if request.method == 'POST':
+        username = request.form['username']
+        email = request.form['email']
+        password = request.form['password']
+
+        if Register.query.filter_by(username=username).first():
+            flash("Username already exists!")
+            return redirect(url_for('signup'))
+
+        new_user = Register(username=username, email=email, password=password)
+        db.session.add(new_user)
+        db.session.commit()
+        flash("Signup successful! Please login.")
+        return redirect(url_for('login'))
+
+    return render_template('signup.html')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+
+        user = Register.query.filter_by(username=username, password=password).first()
+        if user:
+            session['user_id'] = user.id
+            session['username'] = user.username
+            flash("Login successful!")
+            return redirect(url_for('main'))
+        else:
+            flash("Invalid username or password.")
+
+    return render_template('login.html')
+
+
+@app.route('/logout')
+def logout():
+    session.clear()
+    flash("You have been logged out.")
+    return redirect(url_for('login'))
+
+
+# -------------------
+# TODO ROUTES (protected)
+# -------------------
+@app.route('/', methods=['GET', 'POST'])
+@app.route('/main', methods=['GET', 'POST'])
+def main():
+    if 'user_id' not in session:
+        return redirect(url_for('login'))
+
+    if request.method == 'POST':
+        task_content = request.form['task']
+        new_task = Todo(content=task_content)
+        db.session.add(new_task)
+        db.session.commit()
+        return redirect(url_for('main'))
+
+    tasks = Todo.query.order_by(Todo.date_created).all()
+    return render_template('main.html', tasks=tasks)
+
 
 @app.route('/delete/<int:id>')
 def delete(id):
-    task_to_delete=Todo.query.get_or_404(id)
-    try:
-        db.session.delete(task_to_delete)
-        db.session.commit()
-        return redirect('/')
-    except:
-        return 'There was a problem deleting that task'
-@app.route('/update/<int:id>',methods=['GET','POST'])
+    if 'user_id' not in session:
+        return redirect(url_for('login'))
+
+    task_to_delete = Todo.query.get_or_404(id)
+    db.session.delete(task_to_delete)
+    db.session.commit()
+    return redirect(url_for('main'))
+
+
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
 def update(id):
-    task=Todo.query.get_or_404(id)
-    if request.method=='POST':
-        task.content=request.form['task']
-        try:
-            db.session.commit()
-            return redirect('/')
-        except:
-            return 'There was an issue updating your task'
-    else:
-        return render_template('update.html',task=task)
+    if 'user_id' not in session:
+        return redirect(url_for('login'))
+
+    task = Todo.query.get_or_404(id)
+    if request.method == 'POST':
+        task.content = request.form['task']
+        db.session.commit()
+        return redirect(url_for('main'))
+    return render_template('update.html', task=task)
+
+
 if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
     app.run(debug=True)
